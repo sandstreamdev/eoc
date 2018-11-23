84111fcd3c468f7bff5c123073b5e0b86f69a7e7
Adam Klepacz <adamklepacz92@gmail.com>
2018-11-23 16:03:33 +0100

    Change header and add checkbox animations

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -3,10 +3,16 @@ import React from 'react';
 function Header(props) {
   return (
     <div className="app-header">
-      <h1 className="app-header__logo">
-        EOC&nbsp;
-        <span className="app-header__subheader">APP</span>
-      </h1>
+      <div className="app-header__logo">
+        <span className="app-header__text">EOC</span>
+
+        <img
+          className="app-header__icon"
+          src="src/client/assets/images/coffee-solid.svg"
+          alt="Cofee icon"
+        />
+      </div>
+
       <a href="https://sandstream.pl/" className="app-header__link">
         www.sandstream.pl
       </a>
