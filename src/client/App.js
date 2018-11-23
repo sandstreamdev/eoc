b2fd332e6825d6387ad38e8c97a324ddb511e613
Adam Klepacz <adamklepacz92@gmail.com>
2018-11-23 11:58:35 +0100

    Add app header

--- src/client/App.js
+++ src/client/App.js
@@ -1,35 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import ReactImage from './react.png';
 import Header from './components/Header';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: null };
-  }
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
-  render() {
-    const { username } = this.state;
-
-    return (
-      <div>
-        <Header />
-        {username ? (
-          <h1>{`Hello, world, ${username}`}</h1>
-        ) : (
-          <h1>Loading... please wait!</h1>
-        )}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <h1>Hello, world</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Header(props) {
-  return <h1>It is a header!</h1>;
+  return (
+    <div className="app-header">
+      <h1 className="app-header__logo">
+        EOC&nbsp;
+        <span className="app-header__subheader">APP</span>
+      </h1>
+      <a href="https://sandstream.pl/" className="app-header__link">
+        www.sandstream.pl
+      </a>
+    </div>
+  );
 }
 
 export default Header;
