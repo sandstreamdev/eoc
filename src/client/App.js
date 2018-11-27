79f8ba38cac7465dff0efa12c311c40b2c40c060
Adam Klepacz <adamklepacz92@gmail.com>
2018-11-27 09:54:48 +0100

    Fix app.js component

--- src/client/App.js
+++ src/client/App.js
@@ -1,35 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import ReactImage from './react.png';
 import Header from './components/Header';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { username: null };
-  }
-
-  componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
-  }
-
-  render() {
-    const { username } = this.state;
-
-    return (
-      <div>
-        <Header />
-        {username ? (
-          <h1>{`Hello, world, ${username}`}</h1>
-        ) : (
-          <h1>Loading... please wait!</h1>
-        )}
-        <img src={ReactImage} alt="react" />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Header />
+    <h1>Hello, world</h1>
+    <h2>Loading... please wait!</h2>
+    <img src={ReactImage} alt="react" />
+  </div>
+);
 
 export default App;
