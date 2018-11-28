50e6eeb88624704cfe81e638a17e181a9417b10d
Adam Klepacz <adamklepacz92@gmail.com>
2018-11-28 15:35:24 +0100

    Change colors

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -7,19 +7,16 @@ function Header(props) {
     <div className="app-header">
       <div className="app-header__logo">
         <span className="app-header__text">{projectName}</span>
-
         <img
           className="app-header__icon"
           src="src/client/assets/images/coffee-solid.svg"
           alt="Cofee icon"
         />
       </div>
-
       <a href={sandstreamUrl} className="app-header__link">
         Sandstream Development
       </a>
     </div>
   );
 }
-
 export default Header;
