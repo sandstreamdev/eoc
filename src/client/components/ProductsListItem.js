e46db7c3663a49130f549bd64ca102bc5a11e86e
Adam Klepacz <adamklepacz92@gmail.com>
2018-11-28 15:42:51 +0100

    Add small fix to classNames declaration

--- src/client/components/ProductsListItem.js
+++ src/client/components/ProductsListItem.js
@@ -2,32 +2,30 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const ProductListItem = ({ id, image, isArchive, name }) => {
-  const itemStyle = classNames({
-    'products-list__item': true,
-    'products-list__item--red': isArchive,
-    'products-list__item--green': !isArchive
-  });
-
-  return (
-    <li className={itemStyle}>
-      <input
-        className="product-list__input"
-        id={`option${id}`}
-        name={`option${id}`}
-        type="checkbox"
-      />
-      <label
-        className="products-list__label"
-        id={`option${id}`}
-        htmlFor={`option${id}`}
-      >
-        <img src={image} alt="Product icon" className="products-list__icon" />
-        {name}
-      </label>
-    </li>
-  );
-};
+const ProductListItem = ({ id, image, isArchive, name }) => (
+  <li
+    className={classNames({
+      'products-list__item': true,
+      'products-list__item--red': isArchive,
+      'products-list__item--green': !isArchive
+    })}
+  >
+    <input
+      className="product-list__input"
+      id={`option${id}`}
+      name={`option${id}`}
+      type="checkbox"
+    />
+    <label
+      className="products-list__label"
+      id={`option${id}`}
+      htmlFor={`option${id}`}
+    >
+      <img src={image} alt="Product icon" className="products-list__icon" />
+      {name}
+    </label>
+  </li>
+);
 
 ProductListItem.propTypes = {
   id: PropTypes.number.isRequired,
