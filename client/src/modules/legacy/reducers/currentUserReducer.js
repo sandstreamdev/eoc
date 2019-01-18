6d9f28ad9c460b6ad1dd221f215a623ae0b8ba45
Adam Klepacz <adamklepacz92@gmail.com>
2019-01-18 08:49:59 +0100

    Fix redux logout bug

--- client/src/modules/legacy/reducers/currentUserReducer.js
+++ client/src/modules/legacy/reducers/currentUserReducer.js
@@ -1,8 +1,12 @@
 import { SET_CURRENT_USER } from 'modules/legacy/mainActions';
+import { LOGOUT_USER } from 'modules/legacy/UserBar/actions';
 
 const user = (state = null, action) => {
+  console.log(action);
   switch (action.type) {
     case SET_CURRENT_USER:
+      return action.payload;
+    case LOGOUT_USER:
       return action.payload;
     default:
       return state;
