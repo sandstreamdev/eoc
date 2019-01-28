53587f6b084b1527b232d4c6d513ac32cfa17396
Aleksander Fret <alemik@op.pl>
2019-01-28 10:19:07 +0100

    Remove unused variables

--- server/common/variables.js
+++ server/common/variables.js
@@ -1,6 +1,5 @@
 const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
 const ENDPOINT_URL = 'http://localhost:8080';
 const FRONTEND_URL = 'http://localhost:3000';
-const FAILURE_URL = 'http://localhost:3000/failure';
 
-module.exports = { DB_URL, ENDPOINT_URL, FAILURE_URL, FRONTEND_URL };
+module.exports = { DB_URL, ENDPOINT_URL, FRONTEND_URL };
