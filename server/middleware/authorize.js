07e101e122129dddd5167b98786967a248373946
Aleksander Fret <alemik@op.pl>
2019-02-15 08:33:03 +0100

    Refactor code

--- server/middleware/authorize.js
+++ server/middleware/authorize.js
@@ -1,18 +1,17 @@
 const authorize = (req, res, next) => {
   if (!req.user) {
+    const error = {
+      error: true,
+      message: 'Unauthorized access.'
+    };
+
     if (req.cookies['connect.sid']) {
       res.clearCookie('connect.sid');
       res.clearCookie('user');
-      return res.status(403).send({
-        error: true,
-        message:
-          'Your session has ended, you will be redirect to the login page for 5s.'
-      });
+      error.message =
+        'Your session has ended, you will be redirect to the login page for 5s.';
     }
-    return res.status(403).send({
-      error: true,
-      message: 'Unauthorized access.'
-    });
+    res.status(403).send(error);
   }
   return next();
 };
