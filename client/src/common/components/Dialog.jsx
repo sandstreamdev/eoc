2ad795c51d369e98eeb63b42cd172ee77711dd4d
Adam Klepacz <adamklepacz92@gmail.com>
2019-03-01 13:33:13 +0100

    Refactor codeo

--- client/src/common/components/Dialog.jsx
+++ client/src/common/components/Dialog.jsx
@@ -1,36 +1,25 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import Overlay, { OverlayStyleType } from 'common/components/Overlay';
-import CreationForm from 'common/components/CreationForm';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Dialog extends Component {
-  render() {
-    const { children, onCancel, onConfirm, title } = this.props;
-    return (
-      <Fragment>
-        <Overlay type={OverlayStyleType.MEDIUM} />
-        <div className="dialog">
-          <h2 className="dialog__heading">{title && title}</h2>
-          {children}
-          <div className="dialog__footer">
-            <button className="dialog__button" onClick={onCancel} type="button">
-              Cancel
-            </button>
-            <button
-              className="dialog__button"
-              onClick={onConfirm}
-              type="button"
-            >
-              Confirm
-            </button>
-          </div>
-        </div>
-      </Fragment>
-    );
-  }
-}
+const Dialog = ({ children, onCancel, onConfirm, title }) => (
+  <Fragment>
+    <Overlay type={OverlayStyleType.MEDIUM} />
+    <div className="dialog">
+      <h2 className="dialog__heading">{title && title}</h2>
+      {children}
+      <div className="dialog__footer">
+        <button className="dialog__button" onClick={onCancel} type="button">
+          Cancel
+        </button>
+        <button className="dialog__button" onClick={onConfirm} type="button">
+          Confirm
+        </button>
+      </div>
+    </div>
+  </Fragment>
+);
 
 Dialog.propTypes = {
   children: PropTypes.node,
