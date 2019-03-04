24a67c86acdb4823f7dace620d17cb545bd31d55
Adam Klepacz <adamklepacz92@gmail.com>
2019-03-04 14:40:30 +0100

    Restyle authbox

--- client/src/modules/authorization/AuthBox.jsx
+++ client/src/modules/authorization/AuthBox.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 
 import AppLogo from 'common/components/AppLogo';
-import { ENDPOINT_URL } from 'common/constants/variables';
+import { COMPANY_PAGE_URL, ENDPOINT_URL } from 'common/constants/variables';
 
 const AuthBox = () => (
   <div className="authbox">
-    <div className="authbox__intro">
-      <AppLogo />
-      <span className="authbox__subheading">When the fridge is empty...</span>
+    <div className="authbox__left">
+      <h2 className="authbox__heading">End Of Coffee</h2>
+      <p className="authbox__description">
+        The best app to track your office inventory.
+      </p>
     </div>
-    <div className="authbox__buttons">
-      <h1 className="authbox__heading">Sign in:</h1>
-      <a className="google-button" href={`${ENDPOINT_URL}/auth/google`}>
-        <img
-          alt="Google Icon"
-          className="google-button__img"
-          src="client/src/assets/images/google-btn.png"
-        />
-      </a>
+    <div className="authbox__right">
+      <div className="authbox__intro">
+        <AppLogo />
+        <span className="authbox__subheading">When the fridge is empty...</span>
+      </div>
+      <div className="authbox__buttons">
+        <h1 className="authbox__sign-in">Sign in with:</h1>
+        <a className="google-button" href={`${ENDPOINT_URL}/auth/google`}>
+          <img
+            alt="Google Icon"
+            className="google-button__img"
+            src="client/src/assets/images/google-btn.png"
+          />
+        </a>
+      </div>
+      <footer className="authbox__footer">
+        <a className="authbox__link" href={COMPANY_PAGE_URL}>
+          www.sandstream.pl
+        </a>
+      </footer>
     </div>
   </div>
 );
