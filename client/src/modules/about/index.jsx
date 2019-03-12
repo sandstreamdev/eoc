6b16f1447bbf7f0abc79cd5b1799b2b1d2613ea8
Adam Klepacz <adamklepacz92@gmail.com>
2019-03-12 09:50:45 +0100

    Translate page content

--- client/src/modules/about/index.jsx
+++ client/src/modules/about/index.jsx
@@ -23,31 +23,28 @@ const About = () => (
       <div className="about__main">
         <div className="wrapper">
           <article className="about__article">
-            <section>
-              <h2 className="about__heading">Our story...</h2>
+            <h2 className="about__heading">App history</h2>
+            <section className="about__section">
               <p className="about__content">
-                Inspiracja do stworzenia aplikacji End of coffee zrodziła się z
-                potrzeby zarządzania wyposażeniem naszego biura. W początkowej
-                fazie miał to być tylko wewnętrzny projekt szkoleniowy dla
-                nowych developerów ale pomysł rozwinął się i kontynuujemy nasze
-                prace nad aplikacją. Nasi juniorzy mają okazję do zdobycia
-                praktycznego doświadczenia w programowaniu a także w procesie
-                biznesowym tworzenia aplikacji. Jednym z naszych załozeń jest
-                takze stworzenie projektu, którym nasi developerzy bedą mogli
-                pochwalić się w swoim portfolio. Przy pomocy najnowszych
-                technologii tworzymy produkt, który jest odpowiedzią na nasze
-                potrzeby. Być moze i w Twojej firmie przyda Ci się nasze
-                rozwiązanie! Główną funkcjonalnością aplikacji są listy, na
-                których możemy umieszczać produkty, których brakuje obecnie w
-                firmie. W zależności od roli użytkownika na liście, może on
-                dodawać nowe produkty, głosować na nie, odznaczać jako zamówione
-                oraz komentować. W ramach aplikacji można tworzyć także kohorty,
-                które pomagają organizacji i grupowaniu list. Widoczność list w
-                obrębie kohort jest zależna od listy jej członków. Mozliwości
-                edycji, archiwizacji czy usuwania zalezy od roli uzytkownika w
-                danej liście czy kohorcie. Planujemy takze aby aplikacja ta, w
-                przyszlosci stala sie projektem open-source, do którego kazdy
-                bedzie mógł dołozyć swoją cegiełke w postaci pomysłu i kodu.
+                Inspiration to create the End of coffee application was born out
+                of the need to manage our office equipment. In the initial
+                phase, it was to be only an internal training project for new
+                employees. By participating in the project, developers were free
+                to choose technology and tools. The End Of Coffee application
+                can also be an interesting point in the portfolio of every
+                employee who participated in the process of creating this
+                product.
+              </p>
+              <p className="about__content">
+                The main functionality of the application are the lists on which
+                we can place products that are currently missing in the company.
+                Depending on the role of the user on the list, he can add new
+                products, vote for them, mark them as ordered and comment. As
+                part of the application, you can also create cohorts that can
+                help you organize and group your lists. The visibility of lists
+                within a cohort depends on the list of members of a given team.
+                In the future, we plan to make the application an open-source
+                project and anyone who wants to contribute to the project.
               </p>
             </section>
           </article>
