cc37320c9f479fe834e7eded5678d81b32a51b80
Adam Klepacz <adamklepacz92@gmail.com>
2019-03-19 10:07:46 +0100

    Refactor selector

--- client/src/modules/list/index.jsx
+++ client/src/modules/list/index.jsx
@@ -12,7 +12,7 @@ import {
   fetchListData,
   updateList
 } from 'modules/list/model/actions';
-import Dialog from 'common/components/Dialog';
+import Dialog, { DialogContext } from 'common/components/Dialog';
 import FormDialog from 'common/components/FormDialog';
 import { CohortIcon, EditIcon, ArchiveIcon } from 'assets/images/icons';
 import { noOp } from 'common/utils/noOp';
@@ -22,8 +22,7 @@ import ArrowLeftIcon from 'assets/images/arrow-left-solid.svg';
 
 class List extends Component {
   state = {
-    showDialogBox: false,
-    showUpdateForm: false
+    dialogContext: null
   };
 
   componentDidMount() {
@@ -42,27 +41,11 @@ class List extends Component {
     fetchListData(id);
   };
 
-  showDialogBox = () => {
-    this.setState({ showDialogBox: true });
-  };
-
-  hideDialogBox = () => {
-    this.setState({ showDialogBox: false });
-  };
-
   archiveListHandler = listId => () => {
     const { archiveList } = this.props;
     archiveList(listId)
-      .then(this.hideDialogBox)
+      .then(this.handleDialogContext(null)())
       .catch(noOp);
-  };
-
-  hideUpdateForm = () => {
-    this.setState({ showUpdateForm: false });
-  };
-
-  showUpdateForm = () => {
-    this.setState({ showUpdateForm: true });
   };
 
   updateListHandler = listId => (name, description) => {
@@ -73,7 +56,7 @@ class List extends Component {
     description ? (dataToUpdate.description = description) : null;
 
     updateList(listId, dataToUpdate);
-    this.hideUpdateForm();
+    this.handleDialogContext(null)();
   };
 
   checkIfArchived = () => {
@@ -86,8 +69,11 @@ class List extends Component {
     return list && list.isAdmin;
   };
 
+  handleDialogContext = context => () =>
+    this.setState({ dialogContext: context });
+
   render() {
-    const { showDialogBox, showUpdateForm } = this.state;
+    const { dialogContext } = this.state;
     const {
       items,
       match: {
@@ -119,12 +105,12 @@ class List extends Component {
             <Fragment>
               <ToolbarItem
                 mainIcon={<EditIcon />}
-                onClick={this.showUpdateForm}
+                onClick={this.handleDialogContext(DialogContext.UPDATE)}
                 title="Edit list"
               />
               <ToolbarItem
                 mainIcon={<ArchiveIcon />}
-                onClick={this.showDialogBox}
+                onClick={this.handleDialogContext(DialogContext.ARCHIVE)}
                 title="Archive list"
               />
             </Fragment>
@@ -143,14 +129,14 @@ class List extends Component {
           </div>
         )}
         {isArchived && <ArchivedList listId={listId} name={name} />}
-        {showDialogBox && (
+        {dialogContext === DialogContext.ARCHIVE && (
           <Dialog
             onCancel={this.hideDialogBox}
             onConfirm={this.archiveListHandler(listId)}
             title={`Do you really want to archive the ${name} list?`}
           />
         )}
-        {showUpdateForm && (
+        {dialogContext === DialogContext.UPDATE && (
           <FormDialog
             defaultDescription={description}
             defaultName={name}
--- client/src/modules/list/model/selectors.js
+++ client/src/modules/list/model/selectors.js
@@ -1,24 +1,29 @@
 import _pick from 'lodash/pick';
 import _filter from 'lodash/filter';
 import _keyBy from 'lodash/keyBy';
+import { createSelector } from 'reselect';
 
 export const getList = (state, listId) =>
   _pick(state.lists.data, listId)[listId];
-export const getItems = (state, listId) => {
-  const currentList = getList(state, listId);
 
-  if (currentList) {
-    const { items } = currentList;
-    return (
-      items &&
-      items.sort((a, b) =>
-        new Date(a.createdAt).getTime() > new Date(b.createdAt).getTime()
-          ? 1
-          : -1
-      )
-    );
+export const getItems = createSelector(
+  [getList],
+  list => {
+    if (list) {
+      const { items } = list;
+
+      return (
+        items &&
+        items.sort((a, b) =>
+          new Date(a.createdAt).getTime() > new Date(b.createdAt).getTime()
+            ? 1
+            : -1
+        )
+      );
+    }
   }
-};
+);
+
 export const getLists = state => state.lists.data;
 export const getCohortLists = (state, cohortId) =>
   _keyBy(
