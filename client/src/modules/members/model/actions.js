a423fb16080cf652bb3b0510c532bf9fd72bfa59
Aleksander Fret <alemik@op.pl>
2019-03-28 12:54:05 +0100

    Add action

--- client/src/modules/members/model/actions.js
+++ client/src/modules/members/model/actions.js
@@ -1,19 +1,59 @@
 import { UsersActionTypes } from './actionTypes';
-import { getData } from 'common/utils/fetchMethods';
+import { getData, patchData } from 'common/utils/fetchMethods';
 import { createNotificationWithTimeout } from 'modules/notification/model/actions';
 import { MessageType as NotificationType } from 'common/constants/enums';
+import { ENDPOINT_URL } from 'common/constants/variables';
 
 const fetchUsersRequest = () => ({
   type: UsersActionTypes.FETCH_REQUEST
 });
 
 const fetchUsersFailure = () => ({
-  type: UsersActionTypes.FETCH_FAIULRE
+  type: UsersActionTypes.FETCH_FAILURE
 });
 
 const fetchUsersSuccess = data => ({
   type: UsersActionTypes.FETCH_SUCCESS,
   payload: data
+});
+
+const removeCohortUserRequest = () => ({
+  type: UsersActionTypes.REMOVE_REQUEST
+});
+
+const removeCohortUserFailure = () => ({
+  type: UsersActionTypes.REMOVE_FAILURE
+});
+
+const removeCohortUserSuccess = id => ({
+  type: UsersActionTypes.REMOVE_SUCCESS,
+  payload: id
+});
+
+const setAsCohortOwnerRequest = () => ({
+  type: UsersActionTypes.COHORT_OWNER_REQUEST
+});
+
+const setAsCohortOwnerFailure = () => ({
+  type: UsersActionTypes.COHORT_OWNER_FAILURE
+});
+
+const setAsCohortOwnerSuccess = id => ({
+  type: UsersActionTypes.COHORT_OWNER_SUCCESS,
+  payload: id
+});
+
+const setAsCohortMemberRequest = () => ({
+  type: UsersActionTypes.COHORT_MEMBER_REQUEST
+});
+
+const setAsCohortMemberFailure = () => ({
+  type: UsersActionTypes.COHORT_MEMBER_FAILURE
+});
+
+const setAsCohortMemberSuccess = id => ({
+  type: UsersActionTypes.COHORT_MEMBER_SUCCESS,
+  payload: id
 });
 
 export const fetchUsers = () => dispatch => {
@@ -27,6 +67,79 @@ export const fetchUsers = () => dispatch => {
         dispatch,
         NotificationType.ERROR,
         err.message || "Oops, we're sorry, fetching users failed..."
+      );
+    });
+};
+
+export const removeCohortUser = (cohortId, isOwner, userId) => dispatch => {
+  const url = isOwner
+    ? `${ENDPOINT_URL}/cohorts/${cohortId}/remove-owner`
+    : `${ENDPOINT_URL}/cohorts/${cohortId}/remove-member`;
+  dispatch(removeCohortUserRequest());
+  return patchData(url, { userId })
+    .then(resp => resp.json())
+    .then(json => {
+      dispatch(removeCohortUserSuccess(userId));
+      createNotificationWithTimeout(
+        dispatch,
+        NotificationType.SUCCESS,
+        json.message
+      );
+    })
+    .catch(err => {
+      dispatch(removeCohortUserFailure());
+      createNotificationWithTimeout(
+        dispatch,
+        NotificationType.ERROR,
+        err.message
+      );
+    });
+};
+
+export const setAsCohortOwner = (cohortId, userId) => dispatch => {
+  dispatch(setAsCohortOwnerRequest());
+  return patchData(`${ENDPOINT_URL}/cohorts/${cohortId}/set-as-owner`, {
+    userId
+  })
+    .then(resp => resp.json())
+    .then(json => {
+      dispatch(setAsCohortOwnerSuccess(userId));
+      createNotificationWithTimeout(
+        dispatch,
+        NotificationType.SUCCESS,
+        json.message
+      );
+    })
+    .catch(err => {
+      dispatch(setAsCohortOwnerFailure());
+      createNotificationWithTimeout(
+        dispatch,
+        NotificationType.ERROR,
+        err.message
+      );
+    });
+};
+
+export const setAsCohortMember = (cohortId, userId) => dispatch => {
+  dispatch(setAsCohortMemberRequest());
+  return patchData(`${ENDPOINT_URL}/cohorts/${cohortId}/set-as-owner`, {
+    userId
+  })
+    .then(resp => resp.json())
+    .then(json => {
+      dispatch(setAsCohortMemberSuccess(userId));
+      createNotificationWithTimeout(
+        dispatch,
+        NotificationType.SUCCESS,
+        json.message
+      );
+    })
+    .catch(err => {
+      dispatch(setAsCohortMemberFailure());
+      createNotificationWithTimeout(
+        dispatch,
+        NotificationType.ERROR,
+        err.message
       );
     });
 };
--- client/src/modules/members/model/reducer.js
+++ client/src/modules/members/model/reducer.js
@@ -9,6 +9,25 @@ const users = (state = {}, action) => {
         ...state,
         ...action.payload
       };
+    case UsersActionTypes.REMOVE_SUCCESS: {
+      const { [action.payload]: removed, ...rest } = state;
+      return rest;
+    }
+    case UsersActionTypes.COHORT_OWNER_SUCCESS: {
+      const { payload: _id } = action;
+      return {
+        ...state,
+        [_id]: { ...state[_id], isOwner: true }
+      };
+    }
+    case UsersActionTypes.COHORT_MEMBER_SUCCESS: {
+      const { payload: _id } = action;
+      const { isOwner: removed, ...rest } = state[_id];
+      return {
+        ...state,
+        [_id]: rest
+      };
+    }
     default:
       return state;
   }
@@ -16,10 +35,19 @@ const users = (state = {}, action) => {
 
 const isFetching = (state = false, action) => {
   switch (action.type) {
-    case UsersActionTypes.FETCH_SUCCESS:
+    case UsersActionTypes.COHORT_MEMBER_FAILURE:
+    case UsersActionTypes.COHORT_MEMBER_SUCCESS:
+    case UsersActionTypes.COHORT_OWNER_FAILURE:
+    case UsersActionTypes.COHORT_OWNER_SUCCESS:
     case UsersActionTypes.FETCH_FAILURE:
+    case UsersActionTypes.FETCH_SUCCESS:
+    case UsersActionTypes.REMOVE_FAILURE:
+    case UsersActionTypes.REMOVE_SUCCESS:
       return false;
+    case UsersActionTypes.COHORT_MEMBER_REQUEST:
+    case UsersActionTypes.COHORT_OWNER_REQUEST:
     case UsersActionTypes.FETCH_REQUEST:
+    case UsersActionTypes.REMOVE_REQUEST:
       return true;
     default:
       return state;
