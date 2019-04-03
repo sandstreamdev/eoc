e5eaee10dbf1ea7c2c8cbfe6324a3d649a10d8f0
Aleksander Fret <alemik@op.pl>
2019-04-03 08:16:56 +0200

    Fix small issue

--- client/src/common/components/Members/index.jsx
+++ client/src/common/components/Members/index.jsx
@@ -1,8 +1,8 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
 import _map from 'lodash/map';
 import PropTypes from 'prop-types';
 import { Manager } from 'react-popper';
-import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import { DotsIcon, PlusIcon } from 'assets/images/icons';
