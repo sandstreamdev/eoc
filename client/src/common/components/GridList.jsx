9dcddc99d3498006677d03c60b7ed05a0c952139
Aleksander Fret <alemik@op.pl>
2019-04-29 18:19:42 +0200

    Fix small issue

--- client/src/common/components/GridList.jsx
+++ client/src/common/components/GridList.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import _map from 'lodash/map';
