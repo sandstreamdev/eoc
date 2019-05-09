9f30b6278dcae6d8bdb2b52f25803a3e63223fe7
Adam Klepacz <adamklepacz92@gmail.com>
2019-05-09 14:13:39 +0200

    Remove unecessary code

--- client/src/common/components/Toolbar/components/ToolbarLink/ToolbarLink.jsx
+++ client/src/common/components/Toolbar/components/ToolbarLink/ToolbarLink.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const ToolbarLink = ({
-  additionalIconSrc,
-  children,
-  mainIcon,
-  path,
-  title
-}) => (
+const ToolbarLink = ({ additionalIconSrc, mainIcon, path, title }) => (
   <div className="toolbar-link">
     <Link className="toolbar-link__icon-link" title={title} to={path}>
       {mainIcon}
@@ -19,14 +13,12 @@ const ToolbarLink = ({
           src={additionalIconSrc}
         />
       )}
-      {children}
     </Link>
   </div>
 );
 
 ToolbarLink.propTypes = {
   additionalIconSrc: PropTypes.string,
-  children: PropTypes.node,
   mainIcon: PropTypes.node.isRequired,
   path: PropTypes.string.isRequired,
   title: PropTypes.string
