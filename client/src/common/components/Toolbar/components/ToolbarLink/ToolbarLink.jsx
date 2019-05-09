00b080623f89948a066d18f198134faa0d2911e2
Adam Klepacz <adamklepacz92@gmail.com>
2019-05-09 14:07:19 +0200

    Remove company logo and add dashboard redirection on app logo click

--- client/src/common/components/Toolbar/components/ToolbarLink/ToolbarLink.jsx
+++ client/src/common/components/Toolbar/components/ToolbarLink/ToolbarLink.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const ToolbarLink = ({ additionalIconSrc, mainIcon, path, title }) => (
+const ToolbarLink = ({
+  additionalIconSrc,
+  children,
+  mainIcon,
+  path,
+  title
+}) => (
   <div className="toolbar-link">
     <Link className="toolbar-link__icon-link" title={title} to={path}>
       {mainIcon}
@@ -13,12 +19,14 @@ const ToolbarLink = ({ additionalIconSrc, mainIcon, path, title }) => (
           src={additionalIconSrc}
         />
       )}
+      {children}
     </Link>
   </div>
 );
 
 ToolbarLink.propTypes = {
   additionalIconSrc: PropTypes.string,
+  children: PropTypes.node,
   mainIcon: PropTypes.node.isRequired,
   path: PropTypes.string.isRequired,
   title: PropTypes.string
