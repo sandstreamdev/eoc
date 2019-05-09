3e3a36f94ef63902d9cc58ac7a25d7a0036027a4
Aleksander Fret <alemik@op.pl>
2019-05-09 21:46:44 +0200

    Improve date formating, fix new comment styles

--- client/src/common/utils/helpers.js
+++ client/src/common/utils/helpers.js
@@ -31,11 +31,6 @@ export const makeAbortablePromise = promise => {
 
 export const formatDate = string => {
   const date = new Date(string);
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
 
-  return `${day}/${month}/${year} ${hour}:${minutes}`;
+  return date.toLocaleString();
 };
