790a83b33ad73a7e4e44dc936a40f7662861ab8a
Aleksander Fret <alemik@op.pl>
2019-05-09 15:30:31 +0200

    Fix date issue

--- client/src/common/utils/helpers.js
+++ client/src/common/utils/helpers.js
@@ -32,8 +32,8 @@ export const makeAbortablePromise = promise => {
 export const formatDate = string => {
   const date = new Date(string);
   const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDay();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
   const hour = date.getHours();
   const minutes = date.getMinutes();
 
