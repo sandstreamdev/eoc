82689de3b46e572a7ab35188f4bfc5b72d058077
Adam Klepacz <adamklepacz92@gmail.com>
2019-05-25 16:51:24 +0200

    Add update item name for click ouside component

--- client/src/modules/list/components/Items/ListItemName/ListItemName.jsx
+++ client/src/modules/list/components/Items/ListItemName/ListItemName.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { connect } from 'react-redux';
