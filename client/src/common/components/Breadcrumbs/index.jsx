27e804139309b449184ff2c118ea68375f2a41af
Adam Klepacz <adamklepacz92@gmail.com>
2019-05-29 12:10:54 +0200

    Improve breadcrumbs

--- client/src/common/components/Breadcrumbs/index.jsx
+++ client/src/common/components/Breadcrumbs/index.jsx
@@ -5,28 +5,34 @@ import classNames from 'classnames';
 
 import { ChevronRight } from 'assets/images/icons';
 
-const Breadcrumbs = ({ breadcrumbs, isGuest }) => (
-  <div className="wrapper">
-    <nav className="breadcrumbs">
-      <ol
-        className={classNames('breadcrumbs__list', {
-          'breadcrumbs__list--cohort-disabled': isGuest
-        })}
-      >
-        {breadcrumbs.map(breadcrumb => (
-          <Fragment key={breadcrumb.name}>
-            <li className="breadcrumbs__list-item">
-              <Link to={`${breadcrumb.path}`}>{breadcrumb.name}</Link>
-            </li>
-            <li className="breadcrumbs__list-item">
-              <ChevronRight />
-            </li>
-          </Fragment>
-        ))}
-      </ol>
-    </nav>
-  </div>
-);
+const Breadcrumbs = ({ breadcrumbs, isGuest }) => {
+  if (breadcrumbs.length <= 1) {
+    return null;
+  }
+
+  return (
+    <div className="wrapper">
+      <nav className="breadcrumbs">
+        <ol
+          className={classNames('breadcrumbs__list', {
+            'breadcrumbs__list--cohort-disabled': isGuest
+          })}
+        >
+          {breadcrumbs.map(breadcrumb => (
+            <Fragment key={breadcrumb.name}>
+              <li className="breadcrumbs__list-item">
+                <Link to={`${breadcrumb.path}`}>{breadcrumb.name}</Link>
+              </li>
+              <li className="breadcrumbs__list-item">
+                <ChevronRight />
+              </li>
+            </Fragment>
+          ))}
+        </ol>
+      </nav>
+    </div>
+  );
+};
 
 Breadcrumbs.propTypes = {
   breadcrumbs: PropTypes.arrayOf(PropTypes.object),
