f6b755d259c107e9b2807d9ecdbd8a625b37e2a0
Adam Klepacz <adamklepacz92@gmail.com>
2019-06-04 08:52:01 +0200

    Add description to cohort

--- server/seed/demoSeed/generateCohorts.js
+++ server/seed/demoSeed/generateCohorts.js
@@ -7,7 +7,8 @@ const {
 const generateCohorts = (demoUserId, userIds) => [
   {
     _id: ObjectId(),
-    description: 'You are the owner of this cohort.',
+    description:
+      'You are the owner of this cohort. You can create sacks, add members and manage their permissions, modify and archive cohort.',
     isArchived: false,
     memberIds: [demoUserId, ...userIds],
     name: 'Cohort example - owner',
@@ -15,7 +16,8 @@ const generateCohorts = (demoUserId, userIds) => [
   },
   {
     _id: ObjectId(),
-    description: 'You are a member of this cohort.',
+    description:
+      'You are a member of this cohort. You can add sacks, add and invite members.',
     isArchived: false,
     memberIds: [demoUserId, ...userIds],
     name: 'Cohort example - member',
@@ -23,7 +25,8 @@ const generateCohorts = (demoUserId, userIds) => [
   },
   {
     _id: ObjectId(),
-    description: 'You are a member of this cohort.',
+    description:
+      'You are a member of this cohort. You can add sacks, add and invite members.',
     isArchived: true,
     memberIds: [demoUserId],
     name: 'Archived cohort example',
