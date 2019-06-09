d8e442f0fecc8bcd3dfa7f2c8e6b08b0eeea94d4
Aleksander Fret <alemik@op.pl>
2019-06-09 15:20:33 +0200

    Add activities icon

--- client/src/common/components/Activities/index.jsx
+++ client/src/common/components/Activities/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { BellIcon } from 'assets/images/icons';
+import { ActivitiesIcon } from 'assets/images/icons';
 import Dropdown from 'common/components/Dropdown';
 import ActivitiesList from './components/activitiesList';
 
 const Activities = () => (
   <Dropdown
     buttonClassName="activities__button"
-    buttonContent={<BellIcon />}
+    buttonContent={<ActivitiesIcon />}
     dropdownClassName="activities__wrapper"
     dropdownName="activities"
   >
