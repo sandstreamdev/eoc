21320e7597a0dc80d22397d656dbcb794c9df7a4
Adam Klepacz <adamklepacz92@gmail.com>
2019-06-12 14:25:10 +0200

    Translate first element of breadcrumbs

--- client/src/common/components/Breadcrumbs/index.jsx
+++ client/src/common/components/Breadcrumbs/index.jsx
@@ -2,13 +2,19 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
+import { injectIntl } from 'react-intl';
+import _tail from 'lodash/tail';
 
 import { ChevronRight } from 'assets/images/icons';
+import { IntlPropType } from 'common/constants/propTypes';
 
-const Breadcrumbs = ({ breadcrumbs, isGuest }) => {
+const Breadcrumbs = ({ breadcrumbs, isGuest, intl: { formatMessage } }) => {
   if (breadcrumbs.length <= 1) {
     return null;
   }
+
+  const restOfTheBreadcrumbs = _tail(breadcrumbs);
+  const firstBreadcrumb = breadcrumbs[0];
 
   return (
     <div className="wrapper">
@@ -18,7 +24,17 @@ const Breadcrumbs = ({ breadcrumbs, isGuest }) => {
             'breadcrumbs__list--cohort-disabled': isGuest
           })}
         >
-          {breadcrumbs.map(breadcrumb => (
+          <li className="breadcrumbs__list-item">
+            <Link to={`${firstBreadcrumb.path}`}>
+              {formatMessage({
+                id: `common.breadcrumbs.${firstBreadcrumb.name}`
+              })}
+            </Link>
+          </li>
+          <li className="breadcrumbs__list-item">
+            <ChevronRight />
+          </li>
+          {restOfTheBreadcrumbs.map(breadcrumb => (
             <Fragment key={breadcrumb.name}>
               <li className="breadcrumbs__list-item">
                 <Link to={`${breadcrumb.path}`}>{breadcrumb.name}</Link>
@@ -36,7 +52,8 @@ const Breadcrumbs = ({ breadcrumbs, isGuest }) => {
 
 Breadcrumbs.propTypes = {
   breadcrumbs: PropTypes.arrayOf(PropTypes.object),
+  intl: IntlPropType.isRequired,
   isGuest: PropTypes.bool
 };
 
-export default Breadcrumbs;
+export default injectIntl(Breadcrumbs);
