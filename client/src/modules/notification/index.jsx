027e788aa68cdc17979a1302133652e32b6c6ead
Adam Klepacz <adamklepacz92@gmail.com>
2019-06-18 10:19:15 +0200

    Remove unnecessary return

--- client/src/modules/notification/index.jsx
+++ client/src/modules/notification/index.jsx
@@ -10,35 +10,33 @@ import MessageBox from 'common/components/MessageBox';
 import { IntlPropType } from 'common/constants/propTypes';
 import { MessageType as NotificationType } from 'common/constants/enums';
 
-const Notifications = ({ intl: { formatMessage }, notifications }) => {
-  return (
-    <Fragment>
-      {Object.entries(notifications).length > 0 && (
-        <div className="notification">
-          <div className="notification__wrapper">
-            <ul className="notification__list">
-              {_map(notifications, (item, id) => {
-                const {
-                  notification: { notificationId, data }
-                } = item;
-                let message = formatMessage({ id: notificationId }, { data });
+const Notifications = ({ intl: { formatMessage }, notifications }) => (
+  <Fragment>
+    {Object.entries(notifications).length > 0 && (
+      <div className="notification">
+        <div className="notification__wrapper">
+          <ul className="notification__list">
+            {_map(notifications, (item, id) => {
+              const {
+                notification: { notificationId, data }
+              } = item;
+              let message = formatMessage({ id: notificationId }, { data });
 
-                if (item.type === NotificationType.ERROR) {
-                  message += ` ${formatMessage({ id: 'common.try-again' })}`;
-                }
-                return (
-                  <li className="notification__list-item" key={id}>
-                    <MessageBox type={item.type} message={message} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+              if (item.type === NotificationType.ERROR) {
+                message += ` ${formatMessage({ id: 'common.try-again' })}`;
+              }
+              return (
+                <li className="notification__list-item" key={id}>
+                  <MessageBox type={item.type} message={message} />
+                </li>
+              );
+            })}
+          </ul>
         </div>
-      )}
-    </Fragment>
-  );
-};
+      </div>
+    )}
+  </Fragment>
+);
 
 Notifications.propTypes = {
   intl: IntlPropType.isRequired,
