db55b1a03b9ab538bc62abaf052aa6e7ea47373c
Aleksander Fret <alemik@op.pl>
2019-06-24 15:19:43 +0200

    Add sign up route

--- server/routes/authorization.js
+++ server/routes/authorization.js
@@ -6,14 +6,17 @@ const {
   authenticateCallback,
   setDemoUser
 } = require('../config/auth');
+const { signUp } = require('../controllers/userAuth');
 const { logout, sendDemoUser, sendUser } = require('../controllers/userAuth');
 const {
   removeDemoUserChanges
 } = require('../middleware/removeDemoUserChanges');
+const { sendSignUpConfirmationLink } = require('../mailer');
 
 router.post('/demo', setDemoUser, sendDemoUser);
 router.get('/google', authenticate);
 router.get('/google/callback', authenticateCallback, sendUser);
 router.post('/logout', removeDemoUserChanges, logout);
+router.post('/sign-up', signUp, sendSignUpConfirmationLink);
 
 module.exports = router;
