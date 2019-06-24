f8fb0479979c852edd44b278c07bb6fdd7620b36
Aleksander Fret <alemik@op.pl>
2019-06-24 15:18:32 +0200

    Fix action params

--- client/src/modules/authorization/model/actions.js
+++ client/src/modules/authorization/model/actions.js
@@ -59,5 +59,10 @@ export const loginDemoUser = () => dispatch =>
       });
     });
 
-export const signUp = (email, name, password, passwordConfirm) => dispatch =>
-  postData('auth/sign-up', { email, name, password, passwordConfirm });
+export const signUp = (
+  email,
+  username,
+  password,
+  passwordConfirm
+) => dispatch =>
+  postData('auth/sign-up', { email, password, passwordConfirm, username });
