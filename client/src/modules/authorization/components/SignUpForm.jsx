16ee934a62ac6991c11b32079744fee21cc5028c
Aleksander Fret <alemik@op.pl>
2019-06-24 22:40:46 +0200

    Fix code after review

--- client/src/modules/authorization/components/SignUpForm.jsx
+++ client/src/modules/authorization/components/SignUpForm.jsx
@@ -81,9 +81,9 @@ class SignUpForm extends PureComponent {
   };
 
   emailValidator = value => {
-    const { isEmpty, isEmail } = validator;
+    const { isEmail } = validator;
 
-    if (isEmpty(value) || !isEmail(value)) {
+    if (!isEmail(value)) {
       return 'authorization.input.email.invalid';
     }
 
@@ -141,17 +141,14 @@ class SignUpForm extends PureComponent {
     } = this.state;
     const isError = _find(higherLevelErrors, error => error !== '');
 
-    if (
-      isNameValid &&
-      isEmailValid &&
-      isPasswordValid &&
-      isPasswordConfirmValid &&
-      !isError
-    ) {
-      return this.setState({ isFormValid: true });
-    }
-
-    this.setState({ isFormValid: false });
+    return this.setState({
+      isFormValid:
+        isNameValid &&
+        isEmailValid &&
+        isPasswordValid &&
+        isPasswordConfirmValid &&
+        !isError
+    });
   };
 
   handleSignUp = () => {
