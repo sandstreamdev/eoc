577a4c42ce133789b63f4174e66528af08675562
Aleksander Fret <alemik@op.pl>
2019-07-04 13:02:13 +0200

    Fix small issue

--- client/src/modules/list/components/Items/ListItemDescription/ListItemDescription.jsx
+++ client/src/modules/list/components/Items/ListItemDescription/ListItemDescription.jsx
@@ -88,9 +88,11 @@ class ListItemDescription extends PureComponent {
 
   handleShowTextarea = event => {
     const { nodeName } = event.target;
+
     if (nodeName === 'A') {
       return;
     }
+
     event.preventDefault();
 
     this.setState({ isTextareaVisible: true });
