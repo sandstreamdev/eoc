6a52938afdf34528546106bf880e7814bebcf5fc
Aleksander Fret <alemik@op.pl>
2019-07-15 07:46:28 +0200

    Use lodash _upperFirst directly instead of alias

--- client/src/sockets/index.js
+++ client/src/sockets/index.js
@@ -1,31 +1,30 @@
 import io from 'socket.io-client';
-
-import { capitalizeString } from 'common/utils/helpers';
+import _upperFirst from 'lodash/upperFirst';
 
 const socketInstance = io({ forceNew: true });
 
 export const joinRoom = (route, id, userId) => {
-  const room = capitalizeString(route);
+  const room = _upperFirst(route);
   const data = { room: `${route}-${id}`, userId };
 
   socketInstance.emit(`join${room}Room`, data);
 };
 
 export const leaveRoom = (route, id, userId) => {
-  const room = capitalizeString(route);
+  const room = _upperFirst(route);
   const data = { room: `${route}-${id}`, userId };
 
   socketInstance.emit(`leave${room}Room`, data);
 };
 
 export const enterView = (route, userId) => {
-  const view = capitalizeString(route);
+  const view = _upperFirst(route);
 
   socketInstance.emit(`enter${view}View`, userId);
 };
 
 export const leaveView = (route, userId) => {
-  const view = capitalizeString(route);
+  const view = _upperFirst(route);
 
   socketInstance.emit(`leave${view}View`, userId);
 };
