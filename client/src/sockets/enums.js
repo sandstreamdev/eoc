57fd6e110a10e29524b9baaef2018d673d0781dd
Aleksander Fret <alemik@op.pl>
2019-07-15 09:57:58 +0200

    Add socket events for changing roles in cohort

--- client/src/sockets/enums.js
+++ client/src/sockets/enums.js
@@ -2,9 +2,13 @@ import { ListActionTypes } from 'modules/list/model/actionTypes';
 import { CohortActionTypes } from 'modules/cohort/model/actionTypes';
 
 export const ListEvents = Object.freeze({
+  ADD_MEMBER_ROLE_SUCCESS: ListActionTypes.ADD_MEMBER_ROLE_SUCCESS,
+  ADD_OWNER_ROLE_SUCCESS: ListActionTypes.ADD_OWNER_ROLE_SUCCESS,
   ADD_VIEWER_SUCCESS: ListActionTypes.ADD_VIEWER_SUCCESS,
   CREATE_SUCCESS: ListActionTypes.CREATE_SUCCESS,
-  FETCH_META_DATA_SUCCESS: ListActionTypes.FETCH_META_DATA_SUCCESS
+  FETCH_META_DATA_SUCCESS: ListActionTypes.FETCH_META_DATA_SUCCESS,
+  REMOVE_MEMBER_ROLE_SUCCESS: ListActionTypes.REMOVE_MEMBER_ROLE_SUCCESS,
+  REMOVE_OWNER_ROLE_SUCCESS: ListActionTypes.REMOVE_OWNER_ROLE_SUCCESS
 });
 
 export const CohortEvents = Object.freeze({
