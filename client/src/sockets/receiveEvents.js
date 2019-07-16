11c106a8e5121c3735cf53b04e3ca7b774f833aa
Aleksander Fret <alemik@op.pl>
2019-07-16 11:56:08 +0200

    Refactor receiveEvents function

--- client/src/sockets/receiveEvents.js
+++ client/src/sockets/receiveEvents.js
@@ -1,11 +1,16 @@
 import { CohortEvents, ListEvents } from './enums';
 
-export const receiveEvents = (dispatch, socket) => {
+const receiveListEvents = (dispatch, socket) =>
   Object.values(ListEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
+const receiveCohortEvents = (dispatch, socket) =>
   Object.values(CohortEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
+
+export const receiveEvents = (dispatch, socket) => {
+  receiveCohortEvents(dispatch, socket);
+  receiveListEvents(dispatch, socket);
 };
