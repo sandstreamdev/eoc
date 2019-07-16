9d63cbbcb39b6121135ee24bde04a510c3346f44
Adam Klepacz <adamklepacz92@gmail.com>
2019-07-16 12:50:29 +0200

    Extract socket event listeners to separated functions

--- client/src/sockets/receiveEvents.js
+++ client/src/sockets/receiveEvents.js
@@ -1,27 +1,33 @@
 import {
+  CommentEvents,
   ItemsEvents,
   ItemStatusType,
-  CommentEvents,
   ListEvents
 } from './enums';
 
-export const receiveEvents = (dispatch, socket) => {
-  // Items events
+const receiveItemsEvents = (dispatch, socket) =>
   Object.values(ItemsEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
+const receiveItemStatusTypeEvents = (dispatch, socket) =>
   Object.values(ItemStatusType).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
+const receiveCommentEvents = (dispatch, socket) =>
   Object.values(CommentEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
+const receiveListEvents = (dispatch, socket) =>
   Object.values(ListEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
-  // List events
+export const receiveEvents = (dispatch, socket) => {
+  receiveItemsEvents(dispatch, socket);
+  receiveItemStatusTypeEvents(dispatch, socket);
+  receiveCommentEvents(dispatch, socket);
+  receiveListEvents(dispatch, socket);
 };
