62d8555df380446e95236c8d87a76e7ede179f13
Adam Klepacz <adamklepacz92@gmail.com>
2019-07-16 14:39:20 +0200

    Remove events functions

--- client/src/sockets/receiveEvents.js
+++ client/src/sockets/receiveEvents.js
@@ -5,29 +5,20 @@ import {
   ListEvents
 } from './enums';
 
-const receiveItemsEvents = (dispatch, socket) =>
+export const receiveEvents = (dispatch, socket) => {
   Object.values(ItemsEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
-const receiveItemStatusTypeEvents = (dispatch, socket) =>
   Object.values(ItemStatusType).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
-const receiveCommentEvents = (dispatch, socket) =>
   Object.values(CommentEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
 
-const receiveListEvents = (dispatch, socket) =>
   Object.values(ListEvents).forEach(event =>
     socket.on(event, data => dispatch({ type: event, payload: data }))
   );
-
-export const receiveEvents = (dispatch, socket) => {
-  receiveItemsEvents(dispatch, socket);
-  receiveItemStatusTypeEvents(dispatch, socket);
-  receiveCommentEvents(dispatch, socket);
-  receiveListEvents(dispatch, socket);
 };
