c5d6c9d48bf0da83908d0e1ce1cff991f1c2178e
Aleksander Fret <alemik@op.pl>
2019-08-01 08:38:09 +0200

    Fix code after review

--- client/src/common/utils/helpers.js
+++ client/src/common/utils/helpers.js
@@ -17,3 +17,6 @@ export const makeAbortablePromise = promise => {
 };
 
 export const dateFromString = string => new Date(string).toLocaleString();
+
+export const routeBuilder = (route, param = null) =>
+  `/${route}${param ? `/${param}` : ''}`;
--- client/src/sockets/helpers/index.js
+++ client/src/sockets/helpers/index.js
@@ -1,25 +1,22 @@
 import { CohortEvents, ListEvents } from 'sockets/enums';
 import history from 'common/utils/history';
+import { Routes } from 'common/constants/enums';
+import { routeBuilder } from 'common/utils/helpers';
 
 export const listEventsController = (event, data, dispatch) => {
   switch (event) {
-    case ListEvents.LEAVE_ON_TYPE_CHANGE_SUCCESS: {
-      const { cohortId, isCohortMember, listId } = data;
-
-      dispatch({ type: ListEvents.DELETE_SUCCESS, payload: listId });
-
-      return history.replace(
-        `/${isCohortMember ? `cohort/${cohortId}` : 'dashboard'}`
-      );
-    }
+    case ListEvents.LEAVE_ON_TYPE_CHANGE_SUCCESS:
     case ListEvents.REMOVED_BY_SOMEONE: {
       const { cohortId, isCohortMember, listId } = data;
 
       dispatch({ type: ListEvents.DELETE_SUCCESS, payload: listId });
 
-      return history.replace(
-        `/${cohortId && isCohortMember ? `cohort/${cohortId}` : 'dashboard'}`
-      );
+      const url =
+        cohortId && isCohortMember
+          ? routeBuilder(Routes.COHORT, cohortId)
+          : routeBuilder(Routes.DASHBOARD);
+
+      return history.replace(url);
     }
     default:
       return dispatch({ type: event, payload: data });
@@ -33,7 +30,7 @@ export const cohortEventsController = (event, data, dispatch) => {
 
       dispatch({ type: CohortEvents.DELETE_SUCCESS, payload: cohortId });
 
-      return history.replace('/cohorts');
+      return history.replace(routeBuilder(Routes.COHORTS));
     }
     default:
       return dispatch({ type: event, payload: data });
