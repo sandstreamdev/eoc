fde74a43127bcc804b6079cc57100a48c346eb12
Adam Klepacz <adamklepacz92@gmail.com>
2019-08-06 15:33:30 +0200

    Remove mistake

--- client/src/modules/list/model/actions.js
+++ client/src/modules/list/model/actions.js
@@ -261,7 +261,7 @@ export const fetchArchivedListsMetaData = (cohortId = null) => dispatch => {
 };
 
 export const deleteList = (listId, listName, cohortId) => dispatch =>
-  deleteData(`/api/lists/${listId}/delete-item`)
+  deleteData(`/api/lists/${listId}/delete`)
     .then(() => {
       const action = deleteListSuccess({ listId, cohortId });
       const { type, payload } = action;
