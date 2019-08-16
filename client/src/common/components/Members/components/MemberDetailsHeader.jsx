f05e45850df09cc60b2137ca2120badee3c19b15
Adam Klepacz <adamklepacz92@gmail.com>
2019-08-16 11:01:23 +0200

    Extract MemberRole to a separated component

--- client/src/common/components/Members/components/MemberDetailsHeader.jsx
+++ client/src/common/components/Members/components/MemberDetailsHeader.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
 import Avatar from 'common/components/Avatar';
+import MemberRole from 'common/components/Members/components/MemberRole';
 
 const MemberDetailsHeader = ({
   avatarUrl,
@@ -11,40 +11,26 @@ const MemberDetailsHeader = ({
   isGuest,
   isMember,
   isOwner
-}) => {
-  let roleToDisplay = (
-    <FormattedMessage id="common.member-details.role-viewer" />
-  );
-
-  if (isMember) {
-    roleToDisplay = <FormattedMessage id="common.member-details.role-member" />;
-  }
-
-  if (isOwner) {
-    roleToDisplay = <FormattedMessage id="common.member-details.role-owner" />;
-  }
-
-  return (
-    <header className="member-details-header">
-      <div className="member-details-header__avatar">
-        <Avatar
-          avatarUrl={avatarUrl}
-          className="member-details-header__image"
-          name={displayName}
-        />
-      </div>
-      <div>
-        <h3 className="member-details-header__name">{displayName}</h3>
-        <p className="member-details-header__role">
-          {roleToDisplay}
-          {isGuest && isCohortList && (
-            <FormattedMessage id="common.member-details.role-guest" />
-          )}
-        </p>
-      </div>
-    </header>
-  );
-};
+}) => (
+  <header className="member-details-header">
+    <div className="member-details-header__avatar">
+      <Avatar
+        avatarUrl={avatarUrl}
+        className="member-details-header__image"
+        name={displayName}
+      />
+    </div>
+    <div>
+      <h3 className="member-details-header__name">{displayName}</h3>
+      <MemberRole
+        isCohortList={isCohortList}
+        isGuest={isGuest}
+        isMember={isMember}
+        isOwner={isOwner}
+      />
+    </div>
+  </header>
+);
 
 MemberDetailsHeader.propTypes = {
   avatarUrl: PropTypes.string,
