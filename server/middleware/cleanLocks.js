fc4ba5323a981a082f3caae036dc0a1b3b037522
Adam Klepacz <adamklepacz92@gmail.com>
2019-08-21 11:49:05 +0200

    Run unlocks logic in parallel, exit with code 1 when something crashes

--- server/middleware/cleanLocks.js
+++ server/middleware/cleanLocks.js
@@ -1,43 +1,48 @@
 const Cohort = require('../models/cohort.model');
 const List = require('../models/list.model');
 
-const unlockLocks = () =>
+const unlockLocks = () => {
   List.updateMany(
     { locks: { $exists: true } },
     {
       locks: { name: false, description: false }
     }
-  )
-    .then(() =>
-      List.find({ items: { $exists: true } })
-        .exec()
-        .then(lists => {
-          lists.forEach(list => {
-            const { items } = list;
-
-            items.forEach(item => {
-              const { locks } = item;
-
-              locks.name = false;
-              locks.description = false;
-            });
-
-            list.save();
-          });
-        })
-        .catch(err => {
-          throw err;
-        })
-    )
-    .then(() =>
-      Cohort.updateMany(
-        { locks: { $exists: true } },
-        { locks: { name: false, description: false } }
-      ).catch(err => {
-        throw err;
-      })
-    )
+  ).catch(() => {
     // eslint-disable-next-line no-console
-    .catch(() => console.error('Unlocking items failed...'));
+    console.log('Unlocking locks failed...');
+    process.exitCode(1);
+  });
+
+  List.find({ items: { $exists: true } })
+    .exec()
+    .then(lists => {
+      lists.forEach(list => {
+        const { items } = list;
+
+        items.forEach(item => {
+          const { locks } = item;
+
+          locks.name = false;
+          locks.description = false;
+        });
+
+        list.save();
+      });
+    })
+    .catch(() => {
+      // eslint-disable-next-line no-console
+      console.log('Unlocking locks failed...');
+      process.exitCode(1);
+    });
+
+  Cohort.updateMany(
+    { locks: { $exists: true } },
+    { locks: { name: false, description: false } }
+  ).catch(() => {
+    // eslint-disable-next-line no-console
+    console.log('Unlocking locks failed...');
+    process.exitCode(1);
+  });
+};
 
 module.exports = unlockLocks;
