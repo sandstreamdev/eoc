aba6870324b053c83b8be1f3ae37efec9ebfea70
Adam Klepacz <adamklepacz92@gmail.com>
2019-08-26 10:11:57 +0200

    Refactor validatoin methods [WIP]

--- client/src/modules/user/AuthBox/components/PasswordRecoveryForm.jsx
+++ client/src/modules/user/AuthBox/components/PasswordRecoveryForm.jsx
@@ -9,7 +9,7 @@ import { RouterMatchPropType, IntlPropType } from 'common/constants/propTypes';
 import { updatePassword } from 'modules/user/model/actions';
 import ValidationInput from './ValidationInput';
 import PendingButton from 'common/components/PendingButton';
-import { validatePassword } from 'common/utils/helpers';
+import { validatePassword, validateWith } from 'common/utils/helpers';
 
 class PasswordRecoveryForm extends PureComponent {
   constructor(props) {
@@ -89,30 +89,20 @@ class PasswordRecoveryForm extends PureComponent {
 
   passwordValidator = password => {
     const { errors } = this.state;
+    const errorMessageId = validateWith(value => validatePassword(value))(
+      'user.auth.input.password.invalid'
+    )(password);
 
-    if (validatePassword(password)) {
-      this.setState(
-        {
-          errors: {
-            ...errors,
-            passwordError: ''
-          },
-          passwordSuccess: true
+    this.setState(
+      {
+        errors: {
+          ...errors,
+          passwordError: errorMessageId
         },
-        this.comparePasswords
-      );
-    } else {
-      this.setState(
-        {
-          errors: {
-            ...errors,
-            passwordError: 'user.auth.input.password.invalid'
-          },
-          passwordSuccess: false
-        },
-        this.comparePasswords
-      );
-    }
+        passwordSuccess: errorMessageId === ''
+      },
+      this.comparePasswords
+    );
   };
 
   comparePasswords = () => {
