b59db37616b61c26228917bf6eb9cb08a2d82702
Aleksander Fret <alemik@op.pl>
2019-08-28 09:22:34 +0200

    Fix small issue

--- server/models/item.model.js
+++ server/models/item.model.js
@@ -7,7 +7,7 @@ const ItemSchema = new Schema(
   {
     authorId: { type: ObjectId, ref: 'User', required: true },
     description: { type: String, default: '' },
-    editedBy: { type: ObjectId, ref: 'User' },
+    editedBy: { type: String, ref: 'User' },
     isArchived: { type: Boolean, default: false },
     isDeleted: { type: Boolean, default: false },
     isOrdered: { type: Boolean, default: false },
