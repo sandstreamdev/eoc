efcaf71744f8ea8a996d297e033dda9f83392807
Adam Klepacz <adamklepacz92@gmail.com>
2019-09-06 12:48:15 +0200

    Refactor seed data for production server

--- server/seed/demoSeed/generateCohorts.js
+++ server/seed/demoSeed/generateCohorts.js
@@ -10,6 +10,11 @@ const generateCohorts = (demoUserId, userIds) => [
     description:
       'You are the owner of this cohort. You can create sacks, add members and manage their permissions, modify and archive cohort.',
     isArchived: false,
+    isDeleted: false,
+    locks: {
+      description: false,
+      name: false
+    },
     memberIds: [demoUserId, ...userIds],
     name: 'Cohort example - owner',
     ownerIds: [demoUserId]
@@ -19,6 +24,11 @@ const generateCohorts = (demoUserId, userIds) => [
     description:
       'You are a member of this cohort. You can add sacks, add and invite members.',
     isArchived: false,
+    isDeleted: false,
+    locks: {
+      description: false,
+      name: false
+    },
     memberIds: [demoUserId, ...userIds],
     name: 'Cohort example - member',
     ownerIds: [userIds[0]]
@@ -28,6 +38,11 @@ const generateCohorts = (demoUserId, userIds) => [
     description:
       'You are a member of this cohort. You can add sacks, add and invite members.',
     isArchived: true,
+    isDeleted: false,
+    locks: {
+      description: false,
+      name: false
+    },
     memberIds: [demoUserId],
     name: 'Archived cohort example',
     ownerIds: [demoUserId]
