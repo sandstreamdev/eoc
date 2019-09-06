256e12162d2cb27d994f4dcf7804b741156794be
Adam Klepacz <adamklepacz92@gmail.com>
2019-09-06 13:29:32 +0200

    Fix editedby field

--- server/seed/seedDb.js
+++ server/seed/seedDb.js
@@ -33,6 +33,6 @@ const seedDatabase = async () => {
 };
 
 seedDatabase().catch(error => {
-  console.log('☠️  Something went terribly wrong:', error);
+  console.error('☠️  Something went terribly wrong:', error);
   process.exit(1);
 });
