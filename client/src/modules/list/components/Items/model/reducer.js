b01dd5146ad4a1cfbd2f17b17a548b53d9fb84e4
Aleksander Fret <alemik@op.pl>
2019-09-09 09:28:17 +0200

    Fix items reducer

--- client/src/modules/list/components/Items/model/reducer.js
+++ client/src/modules/list/components/Items/model/reducer.js
@@ -145,16 +145,20 @@ const items = (state = {}, action) => {
         payload: { itemId, locks }
       } = action;
       const blockedItem = state[itemId];
-      const { locks: prevLocks } = blockedItem;
-      const updatedLocks = filterDefined(locks);
+      if (blockedItem) {
+        const { locks: prevLocks } = blockedItem;
+        const updatedLocks = filterDefined(locks);
 
-      return {
-        ...state,
-        [itemId]: {
-          ...blockedItem,
-          locks: { ...prevLocks, ...updatedLocks }
-        }
-      };
+        return {
+          ...state,
+          [itemId]: {
+            ...blockedItem,
+            locks: { ...prevLocks, ...updatedLocks }
+          }
+        };
+      }
+
+      return state;
     }
     case CommentActionTypes.ADD_SUCCESS:
     case CommentActionTypes.FETCH_SUCCESS: {
