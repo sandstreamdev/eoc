ef9d96632dddc464c4d0efceef9a9bcb25b4ea5f
Adam Klepacz <adamklepacz92@gmail.com>
2019-09-10 12:38:48 +0200

    Fix small issue

--- server/seed/seedDb.js
+++ server/seed/seedDb.js
@@ -33,6 +33,6 @@ const seedDatabase = async () => {
 };
 
 seedDatabase().catch(error => {
-  console.error('☠️  Something went terribly wrong:', error);
+  console.error('☠️  Something went wrong:', error);
   process.exit(1);
 });
