2790ca7fff071492459bee0da04445c9816e5920
Aleksander Fret <alemik@op.pl>
2019-09-12 09:11:11 +0200

    eoc-464 Fix issue with preventDispatchFromPerformer middleware

--- client/src/app/model/middleware.js
+++ client/src/app/model/middleware.js
@@ -1,16 +1,22 @@
 export const preventDispatchForPerformer = store => next => action => {
-  if (action.payload) {
-    const {
-      payload: { performerId, ...rest },
-      type
-    } = action;
-    const { currentUser: user } = store.getState();
+  if (typeof action !== 'function') {
+    const { payload } = action;
+    if (typeof payload === 'object') {
+      const {
+        payload: { performerId, ...rest },
+        type
+      } = action;
+      const { currentUser: user } = store.getState();
 
-    if (user && performerId === user.id) {
-      return;
+      if (user && performerId && performerId === user.id) {
+        return;
+      }
+
+      return next({
+        type,
+        payload: { ...rest }
+      });
     }
-
-    return next({ type, payload: { ...rest } });
   }
 
   return next(action);
