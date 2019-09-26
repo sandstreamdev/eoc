863048cf59fd82372e6818601259f35101d09cdb
Aleksander Fret <alemik@op.pl>
2019-09-26 17:55:32 +0200

    EOC-467 Create tests for responseWithListDetails helper

--- server/tests/__mocks__/listMock.js
+++ server/tests/__mocks__/listMock.js
@@ -66,6 +66,77 @@ const listMock = [
   }
 ];
 
+const listDetailsMock = {
+  _id: { $oid: '5ccd610f4663533f995b1755' },
+  cohortId: null,
+  favIds: [],
+  isArchived: false,
+  type: 'limited',
+  memberIds: ['5c9dc5a619ae7924074940f4', '5c9dc5a619ae7924074989er'],
+  ownerIds: ['5c9dc5a619ae7924074940f4'],
+  viewersIds: [
+    {
+      _id: '5c9dc5a619ae7924074940f4',
+      avatarUrl: 'https://test.pl/photo.jpeg',
+      displayName: 'John Doe'
+    },
+    {
+      _id: '5c9dc5a619ae7924074989er',
+      avatarUrl: 'https://test.pl/photo.jpeg',
+      displayName: 'William Doe'
+    }
+  ],
+  description: '',
+  name: 'Test list',
+  items: [
+    {
+      _id: { $oid: '5ccd61134663533f995b1756' },
+      authorName: 'Adam Klepacz',
+      authorId: {
+        _id: '5c9dc5a619ae7924074940f4',
+        avatarUrl: 'https://test.pl/photo.jpeg',
+        displayName: 'John Doe'
+      },
+      editedBy: {
+        _id: '5c9dc5a619ae7924074940f4',
+        avatarUrl: 'https://test.pl/photo.jpeg',
+        displayName: 'John Doe'
+      },
+      createdAt: { $date: { $numberLong: '1556963603400' } },
+      description: '',
+      done: false,
+      isArchived: false,
+      name: 'Milk',
+      updatedAt: { $date: { $numberLong: '1556963603400' } },
+      voterIds: []
+    },
+    {
+      _id: { $oid: '5ccd61174663533f995b1758' },
+      authorName: 'Adam Klepacz',
+      authorId: {
+        _id: '5c9dc5a619ae7924074940f4',
+        avatarUrl: 'https://test.pl/photo.jpeg',
+        displayName: 'John Doe'
+      },
+      editedBy: {
+        _id: '5c9dc5a619ae7924074940f4',
+        avatarUrl: 'https://test.pl/photo.jpeg',
+        displayName: 'John Doe'
+      },
+      createdAt: { $date: { $numberLong: '1556963607250' } },
+      description: '',
+      done: false,
+      isArchived: false,
+      name: 'Coffee',
+      updatedAt: { $date: { $numberLong: '1556963607250' } },
+      voterIds: []
+    }
+  ],
+  created_at: { $date: { $numberLong: '1556963599739' } },
+  updatedAt: { $date: { $numberLong: '1556963614753' } },
+  __v: { $numberInt: '0' }
+};
+
 const expectedListProperties = [
   '_id',
   'createdAt',
@@ -100,9 +171,30 @@ const expectedListMetaDataProperties = [
   'unhandledItemsCount'
 ];
 
+const expectedListDetailsProperties = [
+  '_id',
+  'cohortId',
+  'cohortName',
+  'createdAt',
+  'description',
+  'doneItemsCount',
+  'isArchived',
+  'isFavourite',
+  'isGuest',
+  'isMember',
+  'isOwner',
+  'items',
+  'members',
+  'name',
+  'type',
+  'unhandledItemsCount'
+];
+
 module.exports = {
   expectedCohortListProperties,
+  expectedListDetailsProperties,
   expectedListMetaDataProperties,
   expectedListProperties,
+  listDetailsMock,
   listMock
 };
