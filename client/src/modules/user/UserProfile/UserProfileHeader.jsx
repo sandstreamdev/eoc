3271c6b07e599d1b0177422a8c5878bebd9b4705
Adam Klepacz <adamklepacz92@gmail.com>
2019-10-03 10:24:06 +0200

    Refactor UserProfileHeader component to use new helper

--- client/src/modules/user/UserProfile/UserProfileHeader.jsx
+++ client/src/modules/user/UserProfile/UserProfileHeader.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { injectIntl } from 'react-intl';
 
 import Avatar from 'common/components/Avatar';
 import './UserProfileHeader.scss';
+import { formatName } from 'common/utils/helpers';
+import { IntlPropType } from 'common/constants/propTypes';
 
-const UserProfileHeader = ({ avatarUrl, name }) => (
-  <h1 className="user-profile-header">
-    <span>
-      <Avatar
-        avatarUrl={avatarUrl}
-        className="user-profile-header__avatar"
-        name={name}
-      />
-    </span>
-    {name}
-  </h1>
-);
+const UserProfileHeader = ({ avatarUrl, intl: { formatMessage }, name }) => {
+  const formattedName = formatName(name, formatMessage);
+
+  return (
+    <h1 className="user-profile-header">
+      <span>
+        <Avatar
+          avatarUrl={avatarUrl}
+          className="user-profile-header__avatar"
+          name={formattedName}
+        />
+      </span>
+      {formattedName}
+    </h1>
+  );
+};
 
 UserProfileHeader.propTypes = {
   avatarUrl: PropTypes.string,
+  intl: IntlPropType.isRequired,
   name: PropTypes.string
 };
 
-export default UserProfileHeader;
+export default injectIntl(UserProfileHeader);
