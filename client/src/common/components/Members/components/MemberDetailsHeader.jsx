c07d4a63e8b220a49deda1ca4bbf8277c3c764ef
Adam Klepacz <adamklepacz92@gmail.com>
2019-10-03 07:37:24 +0200

    Refactor MemberDetailsHeader component to use new helper

--- client/src/common/components/Members/components/MemberDetailsHeader.jsx
+++ client/src/common/components/Members/components/MemberDetailsHeader.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { injectIntl } from 'react-intl';
 
 import Avatar from 'common/components/Avatar';
 import './MemberDetailsHeader.scss';
+import { formatName } from 'common/utils/helpers';
+import { IntlPropType } from 'common/constants/propTypes';
 
-const MemberDetailsHeader = ({ avatarUrl, displayName, role }) => (
-  <header className="member-details-header">
-    <div className="member-details-header__avatar">
-      <Avatar
-        avatarUrl={avatarUrl}
-        className="member-details-header__image"
-        name={displayName}
-      />
-    </div>
-    <div>
-      <h3 className="member-details-header__name">{displayName}</h3>
-      {role}
-    </div>
-  </header>
-);
+const MemberDetailsHeader = ({
+  avatarUrl,
+  displayName,
+  intl: { formatMessage },
+  role
+}) => {
+  const formattedName = formatName(displayName, formatMessage);
+
+  return (
+    <header className="member-details-header">
+      <div className="member-details-header__avatar">
+        <Avatar
+          avatarUrl={avatarUrl}
+          className="member-details-header__image"
+          name={formattedName}
+        />
+      </div>
+      <div>
+        <h3 className="member-details-header__name">{formattedName}</h3>
+        {role}
+      </div>
+    </header>
+  );
+};
 
 MemberDetailsHeader.propTypes = {
   avatarUrl: PropTypes.string,
   displayName: PropTypes.string.isRequired,
+  intl: IntlPropType.isRequired,
   role: PropTypes.any
 };
 
-export default MemberDetailsHeader;
+export default injectIntl(MemberDetailsHeader);
