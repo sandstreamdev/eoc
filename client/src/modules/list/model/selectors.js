b2801d467bd37293149516f758b3b25b9a136bfd
Przemysław Zalewski <przemyslaw.zalewski@sandstream.pl>
2019-10-07 20:56:04 +0200

    Refactoor list selectors

--- client/src/modules/list/model/selectors.js
+++ client/src/modules/list/model/selectors.js
@@ -1,14 +1,14 @@
-import _pick from 'lodash/pick';
+import { filter, sortBy } from '@sandstreamdev/std/object';
+import { not } from '@sandstreamdev/std/function';
 import _filter from 'lodash/filter';
 import _keyBy from 'lodash/keyBy';
 import { createSelector } from 'reselect';
-import _sortBy from 'lodash/sortBy';
 import _orderBy from 'lodash/orderBy';
 
-export const getList = (state, listId) => _pick(state.lists, listId)[listId];
+export const getList = (state, listId) => state.lists[listId];
 
 export const getItems = createSelector(
-  [getList],
+  getList,
   list => {
     if (list) {
       const { items = {} } = list;
@@ -32,76 +32,81 @@ export const getItems = createSelector(
   }
 );
 
+const isArchived = ({ archived }) => archived;
+const isNotArchived = not(isArchived);
+
+const isDone = ({ done }) => done;
+const isNotDone = not(done);
+
 const getActiveItems = createSelector(
-  [getItems],
-  items => _filter(items, item => !item.isArchived)
+  getItems,
+  filter(isNotArchived)
 );
 
 export const getArchivedItems = createSelector(
-  [getItems],
-  items => _filter(items, item => item.isArchived)
+  getItems,
+  filter(isArchived)
 );
 
 export const getDoneItems = createSelector(
-  [getActiveItems],
-  items => _filter(items, item => item.done)
+  getActiveItems,
+  filter(isDone)
 );
 
 export const getUndoneItems = createSelector(
-  [getActiveItems],
-  items => _filter(items, item => !item.done)
+  getActiveItems,
+  filter(isNotDone)
 );
 
 export const getLists = state =>
   _orderBy(state.lists, el => new Date(el.createdAt).getTime(), ['desc']);
 
-export const getActiveLists = createSelector(
+const isFavourite = ({ isFavourite }) => isFavourite;
+const isNotFavourite = not(isFavourite);
+
+const getOrderedLists = createSelector(
   getLists,
-  lists =>
-    _keyBy(
-      _sortBy(_filter(lists, list => !list.isArchived), el => !el.isFavourite),
-      '_id'
-    )
+  sortBy(isNotFavourite)
+);
+
+export const getActiveLists = createSelector(
+  getOrderedLists,
+  filter(isNotArchived)
 );
 
 export const getArchivedLists = createSelector(
-  getLists,
-  lists =>
-    _keyBy(
-      _sortBy(_filter(lists, list => list.isArchived), el => !el.isFavourite),
-      '_id'
-    )
+  getOrderedLists,
+  filter(isArchived)
 );
+
+const isPrivate = ({ cohortId }) => !cohortId;
+const isNotPrivate = not(isPrivate);
 
 export const getPrivateLists = createSelector(
   getActiveLists,
-  lists => _keyBy(_filter(lists, list => !list.cohortId), '_id')
+  lists => filter(lists, isPrivate)
 );
 
 export const getCohortsLists = createSelector(
   getActiveLists,
-  lists => _keyBy(_filter(lists, list => list.cohortId), '_id')
+  lists => filter(lists, isNotPrivate)
 );
 
-const getCohortLists = (state, id) =>
-  _filter(state.lists, list => list.cohortId === id);
+const getCohortLists = (state, id) => state.lists[id];
+
+const getOrderedCohortLists = createSelector(
+  getCohortLists,
+  sortBy(isNotFavourite)
+);
 
 export const getCohortActiveLists = createSelector(
-  getCohortLists,
-  lists =>
-    _keyBy(
-      _sortBy(_filter(lists, list => !list.isArchived), el => !el.isFavourite),
-      '_id'
-    )
+  getOrderedCohortLists,
+  filter(isNotArchived)
 );
 
 export const getCohortArchivedLists = createSelector(
-  getCohortLists,
-  lists =>
-    _keyBy(
-      _sortBy(_filter(lists, list => list.isArchived), el => !el.isFavourite),
-      '_id'
-    )
+  getOrderedCohortLists,
+  filter(isArchived)
 );
 
 export const getMembers = createSelector(
@@ -109,10 +114,13 @@ export const getMembers = createSelector(
   list => list && list.members
 );
 
+const name = ({ name }) => name;
+
 export const getListsNames = createSelector(
-  [getList, getLists],
+  getList,
+  getLists,
   (currentList, lists) =>
-    _sortBy(_filter(lists, list => list._id !== currentList._id), el => el.name)
+    _sortBy(_filter(lists, list => list._id !== currentList._id), name)
 );
 
 export const getLimits = createSelector(
