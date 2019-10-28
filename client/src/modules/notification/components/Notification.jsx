6ee7a419c127bf1d6f4e80120c43060a0fc09c30
Marek Rozmus <marek.rozmus@sandstream.pl>
2019-10-28 10:57:25 +0100

    Add space between notifications sentences

--- client/src/modules/notification/components/Notification.jsx
+++ client/src/modules/notification/components/Notification.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
@@ -15,7 +15,10 @@ const Notification = ({ data, id, redirect, type }) => {
     <MessageBox type={type}>
       <FormattedMessage id={id} values={values} />
       {type === NotificationType.ERROR && !redirect && (
-        <FormattedMessage id="common.try-again" />
+        <Fragment>
+          <span> </span>
+          <FormattedMessage id="common.try-again" />
+        </Fragment>
       )}
     </MessageBox>
   );
