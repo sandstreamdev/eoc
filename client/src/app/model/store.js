542544ea7f022b1d8b32e6a2f7deb5e3a2117a79
Aleksander Fret <alemik@op.pl>
2019-10-29 10:13:05 +0100

    Restrict most redux-devtools features for production

--- client/src/app/model/store.js
+++ client/src/app/model/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducer';
