2ca71182735156ff2e90f0cc0412b212303a7359
Aleksander Fret <alemik@op.pl>
2019-10-29 10:51:07 +0100

    Disable redux-developer-tools in produciton

--- client/src/app/model/store.js
+++ client/src/app/model/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducer';
