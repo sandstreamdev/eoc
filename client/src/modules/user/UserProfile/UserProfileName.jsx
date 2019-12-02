7293d3b3121a5bb14884e625278fa67f478bd122
Adam Klepacz <adamklepacz92@gmail.com>
2019-12-02 13:17:54 +0100

    Add input validaiton

--- client/src/modules/user/UserProfile/UserProfileName.jsx
+++ client/src/modules/user/UserProfile/UserProfileName.jsx
@@ -1,30 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
+import _trim from 'lodash/trim';
+import isLength from 'validator/lib/isLength';
+import isEmpty from 'validator/lib/isEmpty';
 
-import { CheckIcon } from 'assets/images/icons';
 import './UserProfileName.scss';
+import { validateWith } from 'common/utils/helpers';
 
 const UserProfileName = props => {
   const { name } = props;
   const [errorMessageId, setErrorMessageId] = useState('');
-  const [isFormVisible, setFormVisbility] = useState(false);
+  const [isFormVisible, setFormVisibility] = useState(false);
   const [currentName, setName] = useState(name);
 
-  const showForm = () => setFormVisbility(true);
+  const showForm = () => setFormVisibility(true);
 
-  const hideForm = () => setFormVisbility(false);
+  const hideForm = () => setFormVisibility(false);
+
+  const validateName = () => {
+    let errorMessageId;
+
+    errorMessageId = validateWith(
+      value => !isEmpty(value, { ignore_whitespace: true })
+    )('common.form.required-warning')(currentName);
+
+    if (_trim(currentName)) {
+      errorMessageId = validateWith(value =>
+        isLength(value, { min: 1, max: 32 })
+      )('common.form.field-min-max')(currentName);
+    }
+
+    setErrorMessageId(errorMessageId);
+  };
 
   const handleNameChange = event => {
     const {
       target: { value }
     } = event;
 
-    console.log(value);
-
     setName(value);
   };
+
+  useEffect(() => {
+    validateName();
+  }, [currentName]);
 
   return (
     <>
@@ -40,6 +61,8 @@ const UserProfileName = props => {
                 onSubmit={() => {}}
               >
                 <input
+                  // eslint-disable-next-line jsx-a11y/no-autofocus
+                  autoFocus
                   className="primary-input"
                   onChange={handleNameChange}
                   type="text"
@@ -51,7 +74,7 @@ const UserProfileName = props => {
                   })}
                   type="submit"
                 >
-                  <CheckIcon />
+                  <FormattedMessage id="common.new-comment.save" />
                 </button>
               </form>
               {errorMessageId && (
